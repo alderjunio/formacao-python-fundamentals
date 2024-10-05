3d3945f47fd99d1a8ce81c250b2057e4032bb497
alderjunio <alder.junio@gmail.com>
2024-10-05 12:40:09 -0400

    aula variaveis e constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,16 @@
+nome = "Alder"
+idade = 42
+
+print(nome, idade)
+
+nome = "Helen"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+print(limite_saque_diario)
+
+BRAZILIAN_STATES = ['SP', 'AM', 'RJ', 'RS']
+
+print(BRAZILIAN_STATES)
