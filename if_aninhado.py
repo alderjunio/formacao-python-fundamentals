00e10d38f92e1a7144b31680df6c64bba36895c2
alderjunio <alder.junio@gmail.com>
2024-10-05 17:54:47 -0400

    aula de estruturas condicionais if aninhado

---
+++ if_aninhado.py
@@ -0,0 +1,27 @@
+conta_normal = False
+conta_universitaria = False
+conta_especial = True
+
+saldo = 2000
+saque = 1500
+cheque_especial = 450
+
+if conta_normal:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com uso do cheque especial!")
+    else:
+        print("Não foi possivel realizar o saque! Saldo insuficiente")
+
+elif conta_universitaria:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    else:
+        print("Saldo Insuficiente!")
+elif conta_especial:
+        print("Conta especial selecionada!")     
+else:
+        print("Sistema nao reconheceu seu tipo de conta, entre em contato com seu gerente")
+
+        
