5325336e67e09ab3a0b23f739370e1360a621841
alderjunio <alder.junio@gmail.com>
2024-10-05 21:58:53 -0400

    aula de string com interpolação de variaveis

---
+++ string_2.py
@@ -0,0 +1,7 @@
+nome = "Alder"
+idade = 42
+profissao = "Programador"
+linguagem = "Python"
+PI = 3.14159
+
+print(f"Olá, me chamo {nome}, tenho {idade} anos de idade, trabalho como {profissao} e estou matriculado no curso de {linguagem}, e sei que o valor de Pi é {PI:.2f}.")
