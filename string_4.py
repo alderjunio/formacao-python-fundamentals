e01227efb3cce9694cfc645185797e373f3f8ba8
alderjunio <alder.junio@gmail.com>
2024-10-05 22:13:47 -0400

    aula de string de multiplas linhas

---
+++ string_4.py
@@ -0,0 +1,21 @@
+nome = "Alder"
+
+mensagem = f"""
+olá meu nome é {nome},
+    e estou aprendendo Python
+"""
+
+print(mensagem)
+
+print("""
+    ========== MENU ==========
+
+      1 - Depositar
+      2 - Sacar
+      0 - Sair   
+
+    ==========================
+
+    Obrigado por usar nossos sistemas
+      
+""")
