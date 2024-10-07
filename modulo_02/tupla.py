7b7b74e1644a64dabc4154e4028a3e05bd68a6cd
alderjunio <alder.junio@gmail.com>
2024-10-06 20:47:34 -0400

    aula sobre tuplas

---
+++ modulo_02/tupla.py
@@ -0,0 +1,11 @@
+frutas = ("maçã", "laranja", "uva", "pera", )
+print(frutas[0])
+print(frutas[2])
+print(frutas[-1])
+print(frutas[-3])
+
+letras = tuple("python")
+
+numeros = tuple([1,2,3,4])
+
+pais = ("Brasil",)
